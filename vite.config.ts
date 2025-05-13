7fb5482e60aec51fc568545959f2de1599004ebd
Segueregis <105684859+Segueregis@users.noreply.github.com>
2025-05-12 22:01:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Expor a aplicação em todos os endereços possíveis
+    port: 3000,       // Usar a porta 3000, ou qualquer outra porta que Render suporte
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
